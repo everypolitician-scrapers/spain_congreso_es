2ac4ce03f880d43849497c935f7c020decc12bff
Struan Donald <struan@exo.org.uk>
2016-04-22 12:06:08 +0100

    totally rewrite scraper using capybara
    It seems that there's some sort of session magic occuring which capybara
    handles much better so use that instead of nokogiri.
    
    This now works by creating a memberships table into which it puts every
    historical term/person combination it finds. It then uses that to scrape
    things into data, updating the memberships table with any new
    historical memberships it finds.
    
    It also populates the terms table as it goes with names and dates
    although this does mean the source URL is a bit unhelpful.
    
    If it's already seen a person or a membership then it doesn't scrape
    them again unless either the RESCRAPE_ALL env is set or the
    RESCRAPE_TERM env variable is set to the term of the membership.
    
    Fixes #2

--- scraper.rb
+++ scraper.rb
@@ -2,59 +2,32 @@
 # encoding: utf-8
 
 require 'scraperwiki'
-require 'nokogiri'
-require 'open-uri/cached'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
 require 'pry'
+
+Capybara.default_max_wait_time = 5
+
+
+# images are very slow to load and cause timeouts and
+# as we don't need them skip
+options = {
+    timeout: 60,
+    phantomjs_options: ['--load-images=no']
+}
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
+include Capybara::DSL
+Capybara.default_driver = :poltergeist
+
 
 class String
   def tidy
     self.gsub(/[[:space:]]+/, ' ').strip
-  end
-end
-
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
-
-def scrape_term(term, url)
-  page = noko_for url
-
-  term_details = page.css('div.TITULO_CONTENIDO').text.tidy
-  matched = term_details.match('(^[^\(]*)\s+\(([^\)]*)\)')
-  if matched
-    term_name, dates = matched.captures
-    matched_dates = dates.match('(\d+)\s*-\s*(.*)')
-    if matched_dates
-      start_date, end_date = matched_dates.captures
-    end
-  end
-
-  data = {
-      id: term,
-      name: term_name.tidy,
-      source: url.to_s,
-      start_date: start_date.tidy,
-  }
-
-  if end_date.tidy != 'Actualidad'
-    data[:end_date] = end_date.tidy
-  end
-
-  ScraperWiki.save_sqlite([:id], data, 'terms')
-  scrape_people(term, url)
-end
-
-def scrape_people(term, url)
-  page = noko_for url
-
-  page.css('div#RESULTADOS_DIPUTADOS div.listado_1 ul li a/@href').each do |href|
-    scrape_person(term, URI.join(url, href))
-  end
-
-  pagination = page.css('div.paginacion').first
-  next_page = pagination.xpath(".//a[contains(.,'Página Siguiente')]/@href")
-  unless next_page[0].nil?
-    scrape_people(term, next_page[0].value)
   end
 end
 
@@ -74,102 +47,234 @@ def gender_from(seat)
   return
 end
 
+def save_membership_from_url(name, url)
+  iddiputado = url.to_s.match(/idDiputado=(\d+)/).captures[0]
+  term = url.to_s.match(/idLegislatura=(\d+)/).captures[0]
+  # strip out session id
+  url = url.match(/(.*)_piref[\d_]+\.(next_page.*)/).captures.join('')
+
+  # we can set this to rescrape everything if required
+  unless ENV.key?('RESCRAPE_ALL')
+    # don't save data again
+    cur_name = ScraperWiki::select('name FROM memberships WHERE iddiputado is ? AND term is ?', [iddiputado, term]) rescue nil
+    unless cur_name.nil? or cur_name.empty?
+      return
+    end
+  end
+
+  person = {
+    id: 0,
+    name: name.tidy,
+    term: term,
+    iddiputado: iddiputado,
+    url: url
+  }
+
+  ScraperWiki.save_sqlite([:term, :iddiputado], person, 'memberships')
+end
+
 # use the first term they were elected in and the id from that term as the unique id
 # although for people with only one term the page in question seems to fall over so
 # fall back to the current term and id for those people as it's presumably their first
-def get_unique_id(url, page_term, page_iddiputado)
-    id = ScraperWiki::select('id FROM term_map WHERE iddiputado is ? AND term is ?', [page_iddiputado, page_term]) rescue nil
-    unless id.nil?
-        return id[:id]
-    end
+def get_unique_id(url, page_term, page_iddiputado, name)
+  cur_id = ScraperWiki::select('id FROM memberships WHERE iddiputado is ? AND term is and id <> 0?', [page_iddiputado, page_term]) rescue nil
+  unless cur_id.nil?
+    return cur_id[0][:id]
+  end
+  sleep(1)
 
-    refs = noko_for(url)
+  visit url
 
-    term_map = {}
-    refs.css('div.btn_ficha a/@href').each do |href|
-        term, id = href.to_s.match(/idLegislatura=(\d+).*idDiputado=(\d+)/).captures
+  term_map = {}
+  all('div.all_leg').each do |legislature|
+    within(legislature) do
+      term = nil
+      if legislature.has_css?('div.btn_ficha a')
+        link = find('div.btn_ficha a')
+        href = link['href']
+        # we can't do this as one operation as they don't always appear
+        # in the same order :(
+        term = href.to_s.match(/idLegislatura=(\d+)/).captures[0]
+        id = href.to_s.match(/idDiputado=(\d+)/).captures[0]
         term_map[term.to_i] = id
+        save_membership_from_url(name, href)
+      end
+      if not term.nil? and legislature.has_css?('div.principal')
+        term_div = find('div.principal')
+        name, start_year, end_year = term_div.text.match(/(\w+\s*\w+)\s*\(\s*(\d+)\s*-\s*([^)]*)\)/).captures
+        if end_year.tidy == 'Actualidad'
+          end_year = ''
+        end
+        exists = ScraperWiki::select('id FROM terms WHERE id is ??', [id]) rescue nil
+        if exists.nil?
+          term = {
+            id: term,
+            name: name.tidy,
+            start_date: start_year.tidy,
+            end_date: end_year.tidy,
+            source: 'http://www.congreso.es/',
+          }
+          ScraperWiki.save_sqlite([:id], term, 'terms')
+        end
+      end
     end
+  end
 
-    # the all terms page seems to be very unreliable so if we can't find what we expect
-    # then we should quite rather than trying to make up an incorrect ID
-    if term_map.empty?
-        return nil
+  # the all terms page seems to be very unreliable so if we can't find what we expect
+  # then we should quite rather than trying to make up an incorrect ID
+  if term_map.empty?
+      return nil
+  end
+
+  min_term = term_map.keys.min
+
+  id = "#{min_term}_#{term_map[min_term]}"
+  for term in term_map.keys
+      ScraperWiki.sqliteexecute('update memberships set id = ? where id = 0 and term = ? and iddiputado = ?', [id, term, term_map[term]])
+  end
+  return id
+end
+
+def scrape_people(url)
+  visit url
+
+  all('div#RESULTADOS_DIPUTADOS div.listado_1 ul li a').each do |link|
+    save_membership_from_url(link.text, link['href'])
+  end
+
+  pagination = all('div.paginacion').first
+  next_page = nil
+  if pagination.has_xpath?(".//a[contains(.,'Página Siguiente')]")
+    within (pagination) do
+      next_page = find(:xpath, ".//a[contains(.,'Página Siguiente')]")
     end
+  end
 
-    min_term = term_map.keys.min
+  # the website is a bit fragile to lets not hammer it with requests
+  sleep(2)
+  unless next_page.nil?
+    scrape_people(next_page['href'])
+  end
+end
 
-    id = "#{min_term}_#{term_map[min_term]}"
-    for term in term_map.keys
-        data = {
-            term: term,
-            iddiputado: term_map[term],
-            id: id,
-        }
-        ScraperWiki.save_sqlite([:term, :iddiputado], data, 'term_map')
-    end
-    return id
+def scrape_memberships()
+  memberships = ScraperWiki::select('* FROM memberships')
+  for membership in memberships
+    scrape_person(membership['term'], membership['url'])
+  end
 end
 
 def scrape_person(term, url)
-    person = noko_for(url)
+  iddiputado = url.to_s[/idDiputado=(\d+)/, 1]
 
-    details = person.css('div#curriculum')
-
-    name = details.css('div.nombre_dip').text
-    family_names, given_names = name.split(/,/).map(&:tidy)
-
-    seat, group = details.css('div.texto_dip ul li div.dip_rojo').map(&:text).map(&:tidy)
-    faction, faction_id = group.match(/(.*?) \((.*?)\)/).captures.to_a.map(&:tidy) rescue nil
-
-    unless (fecha_alta = person.xpath('.//div[@class="dip_rojo"][contains(.,"Fecha alta")]')).empty?
-      start_date = fecha_alta.text.match(/(\d+)\/(\d+)\/(\d+)\./).captures.reverse.join("-")
+  unless ENV.key?('RESCRAPE_ALL') or (ENV.key?('RESCRAPE_TERM') and ENV['RESCRAPE_TERM'] == term)
+    # don't scrape data we already have
+    name = ScraperWiki::select('name FROM data WHERE iddiputado is ? AND term is ?', [iddiputado, term]) rescue nil
+    unless name.empty?
+      #name = name[0]['name']
+      #puts "skipping #{name} for #{term}"
+      return
     end
+  end
+  sleep(1)
 
-    unless (causo_baja = person.xpath('.//div[@class="dip_rojo"][contains(.,"Causó baja")]')).empty?
-      end_date = causo_baja.text.match(/(\d+)\/(\d+)\/(\d+)\./).captures.reverse.join("-")
+  # only visit URL if we are collecting the data
+  visit url
+
+  seat, group = all('div#curriculum div.texto_dip ul li div.dip_rojo').map(&:text).map(&:tidy)
+  faction, faction_id = group.match(/(.*?) \((.*?)\)/).captures.to_a.map(&:tidy) rescue nil
+
+  name = find('div#curriculum div.nombre_dip').text
+
+  family_names, given_names = name.split(/,/).map(&:tidy)
+
+  if page.has_xpath?('.//div[@class="dip_rojo"][contains(.,"Fecha alta")]')
+    fecha_alta = find(:xpath, './/div[@class="dip_rojo"][contains(.,"Fecha alta")]')
+    start_date = fecha_alta.text.match(/(\d+)\/(\d+)\/(\d+)\./).captures.reverse.join("-")
+  end
+
+  if page.has_xpath?('.//div[@class="dip_rojo"][contains(.,"Causó baja")]')
+    causo_baja = find(:xpath, './/div[@class="dip_rojo"][contains(.,"Causó baja")]')
+    end_date = causo_baja.text.match(/(\d+)\/(\d+)\/(\d+)\./).captures.reverse.join("-")
+  end
+
+  dob = ''
+  email = ''
+  twitter = ''
+  facebook = ''
+  photo = ''
+  within('div.titular_historico') do
+    dob = all(:xpath, 'following::div/ul/li')[0].text
+  end
+
+  # capybara doesn't support enough xpath to do this
+  # sensibly so we have to do this the longwinded way
+  if page.has_xpath?('//div[class=webperso_dip]/a')
+    all('//div[class=webperso_dip]/a').each do |link|
+      href = link['href']
+      if href.match(/mailto/)
+        email = link.text.tidy
+      end
+      if href.match(/twitter.com/)
+        twitter = href
+      end
+      if href.match(/facebook.com/)
+        facebook = href
+      end
     end
+  end
 
-    iddiputado = url.to_s[/idDiputado=(\d+)/, 1]
-    all_terms_url = person.css('div.soporte_year li a/@href').text.match('.*listadoFichas.*').to_a.first.to_s
-    id = get_unique_id(all_terms_url, term, iddiputado)
-
-    # don't save things f we don't get an id
-    if id.nil?
-        return
+  all('div#datos_diputado').each do |img|
+    within(img) do
+      if img.has_xpath?('.//p[@class="logo_group"]/img[@name="foto"]')
+        photo = find(:xpath, './/p[@class="logo_group"]/img[@name="foto"]')['src'].text
+      end
     end
+  end
 
-    data = {
-        id: id,
-        iddiputado: iddiputado,
-        name: "#{given_names} #{family_names}",
-        sort_name: name,
-        given_name: given_names,
-        family_name: family_names,
-        gender: gender_from(seat),
-        party: person.css('div#datos_diputado p.nombre_grupo').text.tidy,
-        faction_id: faction_id,
-        faction: faction,
-        source: url.to_s,
-        dob: date_of_birth(person.css('div.titular_historico').xpath('following::div/ul/li').text),
-        term: term,
-        start_date: start_date,
-        end_date: end_date,
-        email: person.css('div.webperso_dip a[href*="mailto"]').text.tidy,
-        twitter: person.css('div.webperso_dip a[href*="twitter.com"]/@href').text,
-        facebook: person.css('div.webperso_dip a[href*="facebook.com"]/@href').text,
-        phone: person.css('div.texto_dip').text.match(/Teléfono: (.*)$/).to_a.last.to_s.tidy,
-        fax: person.css('div.texto_dip').text.match(/Fax: (.*)$/).to_a.last.to_s.tidy,
-        photo: person.css('div#datos_diputado p.logo_grupo img[name=foto]/@src').text,
-        constituency: seat[/Diputad. por (.*)\./, 1],
-    }
-    data[:photo] = URI.join(url, data[:photo]).to_s unless data[:photo].to_s.empty?
+  data = {
+    iddiputado: iddiputado,
+    name: "#{given_names} #{family_names}",
+    sort_name: name,
+    given_name: given_names,
+    family_name: family_names,
+    gender: gender_from(seat),
+    party: find('div#datos_diputado p.nombre_grupo').text.tidy,
+    faction_id: faction_id,
+    faction: faction,
+    source: url.to_s,
+    dob: dob,
+    term: term,
+    start_date: start_date,
+    end_date: end_date,
+    email: email,
+    twitter: twitter,
+    facebook: facebook,
+    phone: all('div.texto_dip').map(&:text).join('').match(/Teléfono: (.*)$/).to_a.last.to_s.tidy,
+    fax: all('div.texto_dip').map(&:text).join('').match(/Fax: (.*)$/).to_a.last.to_s.tidy,
+    constituency: seat[/Diputad. por (.*)\./, 1],
+    photo: photo,
+  }
+  data[:photo] = URI.join(url, data[:photo]).to_s unless data[:photo].to_s.empty?
 
-    puts "%s - %s\n" % [ data[:name], data[:id] ]
-    ScraperWiki.save_sqlite([:id, :term], data)
+  all_terms_url = find('div.soporte_year li a')['href'].match('.*listadoFichas.*').to_a.first.to_s
+
+  # it might seem a bit odd to do this only once we've worked out everything
+  # else but doing it this way means we don't need to visit the all terms page
+  # and then go back so it's one less network call per person
+  id = get_unique_id(all_terms_url, term, iddiputado, name)
+
+  # don't save things if we don't get an id
+  if id.nil?
+    #puts "no id so not saving"
+      return
+  end
+
+  data[:id] = id
+
+  #puts "%s - %s\n" % [ data[:name], data[:id] ]
+  ScraperWiki.save_sqlite([:id, :term], data)
 end
 
-(1..11).reverse_each do |term, url|
-  url = 'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados?_piref73_1333056_73_1333049_1333049.next_page=/wc/menuAbecedarioInicio&tipoBusqueda=completo&idLegislatura=%d' % term
-  scrape_term(term, url)
-end
+scrape_people('http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/DiputadosTodasLegislaturas')
+scrape_memberships()
