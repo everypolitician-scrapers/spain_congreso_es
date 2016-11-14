454a4a5fd6aef83b77393e9b8a5deb29508947d3
Chris Mytton <chrismytton@gmail.com>
2016-11-14 13:30:13 +0100

    Add class to represent members' DOB

--- lib/member_page.rb
+++ lib/member_page.rb
@@ -7,6 +7,31 @@ class String
 end
 
 class MemberPage < ScrapedPage
+  class DateOfBirth
+    DATE_REGEX = /(?<day>\d+) de (?<month>[^[:space:]]*) de (?<year>\d+)/
+
+    def initialize(date_string)
+      @date_string = date_string
+    end
+
+    def to_s
+      return if match.nil?
+      "%d-%02d-%02d" % [ match[:year], month(match[:month]), match[:day] ]
+    end
+
+    private
+
+    attr_reader :date_string
+
+    def match
+      @match ||= date_string.match(DATE_REGEX)
+    end
+
+    def month(str)
+      ['','enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'].find_index(str.downcase) or raise "Unknown month #{str}".magenta
+    end
+  end
+
   # Remove session information from url
   def url
     super.to_s.match(/(.*)_piref[\d_]+\.(next_page.*)/).captures.join('')
@@ -42,7 +67,9 @@ class MemberPage < ScrapedPage
   end
 
   field :dob do
-    # TODO: Make a class to encapsulate parsing the DOB.
+    DateOfBirth.new(
+      noko.xpath('.//div[@class="titular_historico"]/following::div/ul/li').first.text
+    ).to_s
   end
 
   field :faction do
