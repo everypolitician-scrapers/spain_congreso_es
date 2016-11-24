daab6d5ab1090fdf74d689044ce01a31347a8bc0
Chris Mytton <chrismytton@gmail.com>
2016-11-24 19:19:31 +0100

    Use ArchiveDecorator in scraper.rb
    This decorator saves pages into the archive.

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require 'scraperwiki'
-require 'uri'
 require 'pry'
 require 'require_all'
 require_rel 'lib'
@@ -8,9 +7,9 @@ require_rel 'lib'
 url = 'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/DiputadosTodasLegislaturas'
 
 loop do
-  page = MembersListPage.new(response: Scraped::Request.new(url: url).response(decorators: [AbsoluteLinks, RemoveSessionInformationFromLinks]))
+  page = MembersListPage.new(response: Scraped::Request.new(url: url).response(decorators: [ArchiveDecorator, AbsoluteLinks, RemoveSessionInformationFromLinks]))
   page.member_urls.each do |member_url|
-    member = MemberPage.new(response: Scraped::Request.new(url: member_url).response(decorators: [AbsoluteLinks, RemoveSessionInformationFromLinks]))
+    member = MemberPage.new(response: Scraped::Request.new(url: member_url).response(decorators: [ArchiveDecorator, AbsoluteLinks, RemoveSessionInformationFromLinks]))
     ScraperWiki.save_sqlite([:name, :term], member.to_h)
   end
   url = page.next_page_url
