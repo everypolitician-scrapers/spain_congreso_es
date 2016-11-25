41464782d9b81687aab2dba67923a6d1702283db
Chris Mytton <chrismytton@gmail.com>
2016-11-25 13:38:28 +0100

    Introduce ArchiveEntry and ArchiveWriter classes
    These offer a bit more flexibility in terms of allowing the user to hook
    into the archiving process. Rather than expect the archiver to know how
    to archive a response, it instead just knows about archive entries.

--- lib/archive.rb
+++ lib/archive.rb
@@ -2,22 +2,74 @@ require 'fileutils'
 require 'scraped_page_archive'
 
 class GitArchive < ScrapedPageArchive::GitStorage
-  def store(response:)
-    uri = URI.parse(response.url.to_s)
-    basename = File.join(uri.host, Digest::SHA1.hexdigest(uri.to_s))
-    git.chdir do
-      FileUtils.mkdir_p(File.dirname(basename))
-      File.write("#{basename}.html", response.body)
-      File.write("#{basename}.yml", ResponseSerializer.new(response: response).to_yaml)
-    end
+  def store(entry:)
+    git.chdir { ArchiveWriter.new(entry: entry).write }
     git.add('.')
-    message = "#{response.status} #{response.url}"
-    git.commit(message)
+    git.commit(entry.commit_message)
     git.push('origin', branch_name)
   end
 end
 
-class ResponseSerializer
+class ArchiveWriter
+  def initialize(entry:)
+    @entry = entry
+  end
+
+  def write
+    body_file.dirname.mkdir unless body_file.dirname.exist?
+    body_file.write(entry.body)
+    metadata_file.dirname.mkdir unless metadata_file.dirname.exist?
+    metadata_file.write(entry.metadata)
+  end
+
+  private
+
+  attr_reader :entry
+
+  def basename
+    @basename ||= entry.key
+  end
+
+  def body_file
+    @body_file ||= Pathname.new("#{basename}.html")
+  end
+
+  def metadata_file
+    @metadata_file ||= Pathname.new("#{basename}.yml")
+  end
+end
+
+class ArchiveEntry
+  def initialize(response:)
+    @response = response
+  end
+
+  def key
+    @key ||= File.join(uri.host, Digest::SHA1.hexdigest(uri.to_s))
+  end
+
+  def body
+    @body ||= response.body
+  end
+
+  def metadata
+    @metadata ||= ResponseMetadata.new(response: response).to_yaml
+  end
+
+  def commit_message
+    @commit_message ||= "#{response.status} #{response.url}"
+  end
+
+  private
+
+  attr_reader :response
+
+  def uri
+    @uri ||= URI.parse(response.url.to_s)
+  end
+end
+
+class ResponseMetadata
   def initialize(response:)
     @response = response
   end
@@ -52,7 +104,8 @@ class ArchiveDecorator < Scraped::Response::Decorator
 
   def decorated_response
     # Archive the response
-    ARCHIVE.store(response: response)
+    entry = ArchiveEntry.new(response: response)
+    ARCHIVE.store(entry: entry)
     super
   end
 end
