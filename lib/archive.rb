707d07c145d6761885cf48387b2fa87a86bca045
Chris Mytton <chrismytton@gmail.com>
2016-11-28 11:45:13 +0100

    WIP: Broken version of saving response to archive

--- lib/archive.rb
+++ lib/archive.rb
@@ -1,41 +1,98 @@
+require 'English'
 require 'fileutils'
 require 'scraped_page_archive'
 
-class GitArchive < ScrapedPageArchive::GitStorage
+class GitArchive
+  def initialize(git_url: git_remote_get_url_origin, branch: 'scraped-pages-archive')
+    @git_url = git_url
+    @branch = branch
+  end
+
   def store(entry:)
-    git.chdir { ArchiveWriter.new(entry: entry).write }
+    git.chdir do
+      entry.files.each do |path, content|
+        FileUtils.mkdir_p(File.dirname(path))
+        File.write(path, content)
+      end
+    end
     git.add('.')
     git.commit(entry.commit_message)
-    git.push('origin', branch_name)
-  end
-end
-
-class ArchiveWriter
-  def initialize(entry:)
-    @entry = entry
-  end
-
-  def write
-    body_file.dirname.mkdir unless body_file.dirname.exist?
-    body_file.write(entry.body)
-    metadata_file.dirname.mkdir unless metadata_file.dirname.exist?
-    metadata_file.write(entry.metadata)
+    git.push('origin', branch)
   end
 
   private
 
-  attr_reader :entry
+  attr_reader :git_url, :branch
 
-  def basename
-    @basename ||= entry.key
+  def tmpdir
+    @tmpdir ||= Dir.mktmpdir
   end
 
-  def body_file
-    @body_file ||= Pathname.new("#{basename}.html")
+  def git
+    @git ||= Git.clone(git_url, tmpdir).tap do |g|
+      g.config('user.name', "scraped_page_archive gem #{ScrapedPageArchive::VERSION}")
+      g.config('user.email', "scraped_page_archive-#{ScrapedPageArchive::VERSION}@scrapers.everypolitician.org")
+      if g.branches[branch] || g.branches["origin/#{branch}"]
+        g.checkout(branch)
+      else
+        g.chdir do
+          # FIXME: It's not currently possible to create an orphan branch with ruby-git
+          # @see https://github.com/schacon/ruby-git/pull/140
+          system("git checkout --orphan #{branch}")
+          system('git rm --quiet -rf .')
+        end
+        g.commit('Initial commit', allow_empty: true)
+      end
+    end
   end
 
-  def metadata_file
-    @metadata_file ||= Pathname.new("#{basename}.yml")
+  def git_remote_get_url_origin
+    remote_url = `git config remote.origin.url`.chomp
+    return nil unless $CHILD_STATUS.success?
+    remote_url
+  end
+end
+
+class ArchivePath
+  def initialize(uri)
+    @uri = URI.parse(uri.to_s)
+  end
+
+  def to_s
+    File.join(uri.host, Digest::SHA1.hexdigest(uri.to_s))
+  end
+
+  private
+
+  attr_reader :uri
+end
+
+# Gets a response back out of the archive.
+class ArchiveStrategy < Scraped::Request::Strategy
+  def response
+    Scraped::Response.new(body: body, status: status, headers: headers, url: url)
+  end
+
+  private
+
+  def status
+    @status ||= metadata['response']['status']['code'].to_i
+  end
+
+  def headers
+    metadata['response']['headers'].to_h
+  end
+
+  def body
+    @body ||= File.read("#{key}.html")
+  end
+
+  def metadata
+    @metadata ||= YAML.load_file("#{key}.yml")
+  end
+
+  def key
+    @key ||= ArchivePath.new(url)
   end
 end
 
@@ -44,16 +101,11 @@ class ArchiveEntry
     @response = response
   end
 
-  def key
-    @key ||= File.join(uri.host, Digest::SHA1.hexdigest(uri.to_s))
-  end
-
-  def body
-    @body ||= response.body
-  end
-
-  def metadata
-    @metadata ||= ResponseMetadata.new(response: response).to_yaml
+  def files
+    {
+      "#{key}.html" => response.body,
+      "#{key}.yml" => ResponseMetadata.new(response: response).to_yaml,
+    }
   end
 
   def commit_message
@@ -64,8 +116,8 @@ class ArchiveEntry
 
   attr_reader :response
 
-  def uri
-    @uri ||= URI.parse(response.url.to_s)
+  def key
+    @key ||= ArchivePath.new(response.url)
   end
 end
 
@@ -103,9 +155,7 @@ class ArchiveDecorator < Scraped::Response::Decorator
   ARCHIVE = GitArchive.new
 
   def decorated_response
-    # Archive the response
-    entry = ArchiveEntry.new(response: response)
-    ARCHIVE.store(entry: entry)
+    ARCHIVE.store(entry: ArchiveEntry.new(response: response))
     super
   end
 end
--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,7 @@ require_rel 'lib'
 url = 'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/DiputadosTodasLegislaturas'
 
 loop do
-  page = MembersListPage.new(response: Scraped::Request.new(url: url).response(decorators: [ArchiveDecorator, AbsoluteLinks, RemoveSessionInformationFromLinks]))
+  page = MembersListPage.new(response: Scraped::Request.new(url: url, strategies: [ArchiveStrategy]).response(decorators: [ArchiveDecorator, AbsoluteLinks, RemoveSessionInformationFromLinks]))
   page.member_urls.each do |member_url|
     member = MemberPage.new(response: Scraped::Request.new(url: member_url).response(decorators: [ArchiveDecorator, AbsoluteLinks, RemoveSessionInformationFromLinks]))
     ScraperWiki.save_sqlite([:name, :term], member.to_h)
