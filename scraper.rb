b5a3b7ed298648e0855d63ae9d3d373e455848ce
Oliver Denman <oliver@mysociety.org>
2017-03-13 09:44:48 +0000

    Scrape all memberships

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,14 @@
+# Each member has a profile page and a list of memberships.
+# The most recent membership for a given member is captured
+# from the profile page. Additional memberships are captured
+# separately.
+
 # frozen_string_literal: true
 require 'scraperwiki'
 require 'pry'
 require_relative 'lib/members_list_page'
 require_relative 'lib/member_page'
+require_relative 'lib/membership_list'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
@@ -12,15 +18,43 @@ def scrape(h)
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-def scrape_members(url)
+def member_urls(url, urls = [])
   page = scrape(url => MembersListPage)
-  page.member_urls.map do |member_url|
-    data = scrape(member_url => MemberPage).to_h
-    puts data.reject { |k, v| v.to_s.empty? }.sort_by { |k, v| k }.to_h
-    ScraperWiki.save_sqlite([:name, :term], data)
+  urls += page.member_urls
+  return urls if page.next_page_url.nil?
+  member_urls(page.next_page_url, urls)
+end
+
+# Get a list of URLs for all the members listed.
+members_list = 'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/DiputadosTodasLegislaturas'
+urls = member_urls(members_list).uniq
+puts "Found #{urls.count} member pages."
+
+# Rejects fields not to be merged with `other_memberships`.
+def person_data(data)
+  unwanted_keys = %i(party faction faction_id start_date end_date constituency term)
+  data.reject do |k, _v|
+    unwanted_keys.include? k
   end
-  scrape_members(page.next_page_url) unless page.next_page_url.nil?
+end
+
+# Scrape the profile page for each member. (`latest_membership`)
+# Scrape the 'Todas las legislaturas' list of each member. (`other_memberships`)
+# Merge each 'other_membership' (dropping the membership listed on the profile page)
+# with data from the profile page. (`all_memberships`)
+# Add the membership captured from the profile page to `all_memberships`
+count = 0
+merged_memberships = urls.flat_map do |url|
+  puts count += 1
+  latest_membership = scrape(url => MemberPage).to_h
+  other_memberships = MembershipList.new(latest_membership[:source]).memberships.map(&:to_h).drop(1)
+  all_memberships = other_memberships.map do |mem|
+    mem.merge(person_data(latest_membership))
+  end
+  all_memberships.push latest_membership
 end
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_members('http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados/DiputadosTodasLegislaturas')
+merged_memberships.each do |mem|
+  ScraperWiki.save_sqlite(%i(name iddiputado term), mem)
+end
